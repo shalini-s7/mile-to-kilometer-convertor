dea210a1ae0834682c5e01cba690b14af01a73dc
shalini-s7 <srisaishalini12345@gmail.com>
2026-01-16 15:38:49 +0530

    first

---
+++ main.py
@@ -0,0 +1,24 @@
+
+from tkinter import *
+def miles_to_km():
+    miles= float(mile_input.get())
+    km = round(miles *1.689)
+    kilometer_result_label.config(text=f"{km}")
+
+window = Tk()
+window.title("MILESTONE COVERTOR")
+window.config(padx=20,pady=20)
+mile_input= Entry()
+mile_input.grid(column=1,row=0)
+mile_input.grid(column=1,row=0)
+mile_label = Label(text="miles")
+mile_label.grid(column=2,row=0)
+is_equal_to = Label(text="is equal to")
+is_equal_to.grid(column=0,row=1)
+kilometer_result_label = Label(text="0")
+kilometer_result_label.grid(column=1,row=1)
+kilometer_label = Label(text="km")
+kilometer_label.grid(column=2,row=1)
+calculate_button = Button(text="calculate",command=miles_to_km)
+calculate_button.grid(column=1,row=2)
+window.mainloop()
